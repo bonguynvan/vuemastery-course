87554b17c9543dd4b766d853331ee84c21c8e5ae
bonguyen <bonguynvan289@gmail.com>
2023-04-25 22:58:41 +0800

    upload code

---
+++ src/components/EventCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} - {{ event.date }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  border: 1px solid #39495c;
+  cursor: pointer;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
