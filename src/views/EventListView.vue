bd088a5f86b0ac9988dbf9eb5080278813883c6e
bonguyen <bonguynvan289@gmail.com>
2023-05-02 18:47:59 +0800

    pagination with vue router

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,27 +1,64 @@
 <script setup>
-  import EventCard from '@/components/EventCard.vue'
-  import { onMounted, ref } from 'vue'
-  import eventService from '@/services/event.service.js';
-  const events = ref(null)
-  onMounted( async () => {
-    const results = await eventService.getEvents()
-    if(results) events.value = results.data
-    console.log(events.value)
-  }) 
+import EventCard from '@/components/EventCard.vue'
+import { onMounted, ref, watchEffect, computed } from 'vue'
+import eventService from '@/services/event.service.js'
+const events = ref(null)
+const totalEvents = ref(0)
+const props = defineProps({
+  page: Number
+})
+onMounted(() => {
+  watchEffect(async () => {
+    events.value = null
+    const results = await eventService.getEvents(2, props.page)
+    if (results) {
+      events.value = results.data;
+      totalEvents.value = results.headers['x-total-count']
+    }
+    console.log(props.page)
+  })
+})
+const hasNext = computed(() => {
+  const totalPage = Math.ceil(totalEvents.value/2)
+  return props.page < totalPage
+})
 </script>
 
 <template>
   <div class="home">
     <h1>Events for Good</h1>
     <div class="event">
-      <EventCard v-for="event in events" :key="event.id" :event="event"/>
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
+    <div class="pagination">
+      <RouterLink id="prev" :to="{ name: 'event-list', query: { page: props.page - 1 } }" v-if="props.page != 1">
+        Previous
+      </RouterLink>
+      <RouterLink id="next" :to="{ name: 'event-list', query: { page: props.page + 1 } }" v-if="hasNext">Next</RouterLink>
     </div>
   </div>
 </template>
 <style scoped>
-  .event {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.event {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.pagination {
+  display: inline-flex;
+  width: 290px;
+}
+.pagination #prev {
+  text-align: left;
+}
+
+.pagination #next {
+  text-align: right;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #232323
+}
+
 </style>
